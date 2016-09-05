8dec1456b54a4a937cc802cdc2a22bdeaa72f818
Kenny Au <ken@gfycat.com>
2016-09-04 21:09:01 -0600

    Script to replace quoted commas in csv.

---
+++ python/csv/replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import csv
+
+with open('input.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        for i in range(0, len(row)):
+            row[i] = row[i].replace(',', '')
+        print ','.join(row)
