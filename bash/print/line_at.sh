13e68dac17779768caf9857fd6176bdc92c89729
Kenny Au <ken@gfycat.com>
2016-09-05 17:32:08 -0600

    Added bash for each line.

--- bash/print/line_at.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sed "2;d" input.txt
--- python/argparse/arguments.py
+++ python/argparse/arguments.py
@@ -6,7 +6,7 @@ def main():
     description = 'argparse example'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-a', '--a', help='an argument', required=True)
-    parser.parse_args()
+    args = parser.parse_args()
 
 if __name__ == '__main__':
     main()
