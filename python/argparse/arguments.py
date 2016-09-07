4adb899a5769abcfa233080d2b5bc3de336f3b89
Kenny Au <ken@gfycat.com>
2016-09-06 19:30:34 -0600

    Updated argparse example with more cases.

--- python/argparse/arguments.py
+++ python/argparse/arguments.py
@@ -5,7 +5,14 @@ import argparse
 def main():
     description = 'argparse example'
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('-a', '--a', help='an argument', required=True)
+    # result is stored in 'args.arg'.
+    parser.add_argument('-a', '--arg', help='an argument', required=True)
+    # boolean result is stored in 'args.flag'.
+    parser.add_argument('--flag', action='store_true', help='a flag')
+    # integer result is stored in 'args.integer'.
+    parser.add_argument('--int', default=0, dest='integer', type=int)
+    # result is stored in 'args.choice'.
+    parser.add_argument('--choice', choices=['one', 'two'], default='one')
     args = parser.parse_args()
 
 if __name__ == '__main__':
