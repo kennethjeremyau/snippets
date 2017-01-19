73a32e97ae76cbf58243c42cee8e2bd9a36d98e9
Kenny Au <kennethjeremyau@gmail.com>
2017-01-19 13:45:03 -0700

    Added python multiprocess example.

---
+++ python/multiprocess/process.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import argparse
+import csv
+import multiprocessing
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', help='number of workers', default=1,
+        dest='num_workers', type=int)
+    args = parser.parse_args()
+
+    workers = []
+    for i in range(0, args.num_workers):
+        worker = multiprocessing.Process(
+            target=start_worker,
+            args=(i,)
+        )
+        workers.append(worker)
+        worker.start()
+
+    for worker in workers:
+        worker.join()
+
+
+def start_worker(id):
+    with open('{}.txt'.format(id), 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Output for worker {}.'.format(id)])
+
+
+if __name__ == '__main__':
+    main()
