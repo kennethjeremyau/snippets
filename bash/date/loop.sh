0591e9e92dbc9a3cffce46dd0724519ff1d80fdf
Kenny Au <kennethjeremyau@gmail.com>
2017-03-16 23:43:58 -0600

    Update to bash loop date.

--- bash/date/loop.sh
+++ bash/date/loop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-d=2015-01-01
-while [ "$d" != 2015-02-20 ]; do 
-    echo $d
-    d=$(gdate -I -d "$d + 1 day")
+d="2017-02-14T07:00:00+00:00"
+while [ "$d" != "2017-03-08T23:00:00+00:00" ]; do 
+    echo $(gdate -d "$d" "+%Y-%m-%d-%H-%M-%S")
+    d=$(TZ=UTC gdate -Iseconds -d "$d + 1 hour")
 done
