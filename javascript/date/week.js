d04ac2fbfbb5ab6feae86211dd7b3b49a14db23f
Kenny Au <kennethjeremyau@gmail.com>
2018-01-02 15:07:40 -0700

    Added js script to get week of year.

---
+++ javascript/date/week.js
@@ -0,0 +1,8 @@
+function getWeekOfYear(d) {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+}
+
+console.log(getWeekOfYear(new Date()));
