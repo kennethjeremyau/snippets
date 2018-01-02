5f35281d0022fbf4dbee5f8c991287f0c3b67736
Kenny Au <kennethjeremyau@gmail.com>
2018-01-02 15:07:06 -0700

    Added segment args to dynamodb scan script.

--- python/aws/dynamodb/scan.py
+++ python/aws/dynamodb/scan.py
@@ -2,7 +2,13 @@
 
 from boto import dynamodb2
 from boto.dynamodb2.table import Table
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--segment', default=0, type=int)
+parser.add_argument('--total-segments', default=1, type=int)
+args = parser.parse_args()
 
 table = Table('tablename', connection=dynamodb2.connect_to_region('us-east-1'))
-for row in table.scan():
+for row in table.scan(segment=args.segment, total_segments=args.total_segments):
     print row['id']
