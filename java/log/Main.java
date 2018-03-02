54cef4c400aa4c5b57bfe338b25768ad8bbb876d
Kenny Au <kennethjeremyau@gmail.com>
2018-03-02 10:01:09 -0700

    Added java logging example.

---
+++ java/log/Main.java
@@ -0,0 +1,9 @@
+import java.util.logging.Logger;
+
+class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        logger.warning("Warning message");
+    }
+}
