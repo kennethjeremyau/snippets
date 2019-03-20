73411d245a80f90c7e160533614f3495ced00497
Kenny Au <kennethjeremyau@gmail.com>
2019-03-20 10:37:07 -0600

    Turned exponential backoff into a decorator.

--- python/backoff/exponential_backoff.py
+++ python/backoff/exponential_backoff.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 
+import functools
 import time
 
-MAX_ATTEMPTS = 5
+def exponential_backoff(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        MAX_ATTEMPTS = 5
+        attempt = 1
+        while attempt <= MAX_ATTEMPTS:
+            success = True
+            try:
+                output = func(*args, **kwargs)
+            except Exception as e:
+                success = False
+            if success:
+                break
+            if attempt == MAX_ATTEMPTS:
+                raise e
+            print 'attempt {}'.format(attempt)
+            time.sleep(2 ** attempt)
+            attempt += 1
+        return output
+    return wrapper
 
-attempt = 1
-while attempt <= MAX_ATTEMPTS:
-    success = True
-    try:
-        raise Exception('SAMPLE ERROR')
-    except:
-        success = False
-    if success:
-        break
-    if attempt == MAX_ATTEMPTS:
-        print 'FINISHED'
-        break
-    print attempt
-    time.sleep(2 ** attempt)
-    attempt += 1
+@exponential_backoff
+def func():
+    raise Exception('SAMPLE ERROR')
+
+func()
