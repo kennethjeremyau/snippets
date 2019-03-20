b85fce8da0be8736d2516e898c1982f1ab9eb7df
Kenny Au <kennethjeremyau@gmail.com>
2019-03-20 10:13:36 -0600

    Expanded exponential backoff.

--- python/backoff/exponential_backoff.py
+++ python/backoff/exponential_backoff.py
@@ -6,6 +6,16 @@ MAX_ATTEMPTS = 5
 
 attempt = 1
 while attempt <= MAX_ATTEMPTS:
+    success = True
+    try:
+        raise Exception('SAMPLE ERROR')
+    except:
+        success = False
+    if success:
+        break
+    if attempt == MAX_ATTEMPTS:
+        print 'FINISHED'
+        break
     print attempt
     time.sleep(2 ** attempt)
     attempt += 1
