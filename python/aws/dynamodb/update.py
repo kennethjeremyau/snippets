632e3e0a0f1e1dd226dd8d2e0255d27e98d8a3c0
Kenny Au <kennethjeremyau@gmail.com>
2019-04-05 12:14:13 -0600

    Fixed python dynamodb update.

--- python/aws/dynamodb/update.py
+++ python/aws/dynamodb/update.py
@@ -6,8 +6,10 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('TABLE')
 try:
     table.update_item(
-        Item={ 'id': 'ID' },
-        UpdateExpression='SET attr = 1',
+        Key={ 'id': 'ID' },
+        UpdateExpression='SET #n = :v',
+        ExpressionAttributeNames={ '#n': 'ATTRIBUTE_NAME' },
+        ExpressionAttributeValues={ ':v': 1 },
         ConditionExpression='attribute_exists(id)',
     )
 except Exception as e:
