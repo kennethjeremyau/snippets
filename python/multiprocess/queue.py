8fa4f1c03f04aa5a257685ec65eb0bf66544a9ce
Kenny Au <kennethjeremyau@gmail.com>
2019-04-24 11:33:11 -0600

    Greatly improved the queue multithreading example.

--- python/multiprocess/queue.py
+++ python/multiprocess/queue.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python
 
-from Queue import Queue
-from threading import Thread
+import Queue
+import threading
 
 NUM_WORKERS = 10
 
 
-def worker():
-    while True:
-        item = q.get()
-        print item
-        q.task_done()
+def worker(done):
+    while not done.is_set():
+        try:
+            item = q.get_nowait()
+            print item
+            q.task_done()
+        except Queue.Empty:
+            pass
+    print 'thread exiting'
 
-q = Queue()
+
+q = Queue.Queue()
+done_event = threading.Event()
 for i in range(NUM_WORKERS):
-    t = Thread(target=worker)
-    t.daemon = True
+    t = threading.Thread(target=worker, args=(done_event,))
+    # Can use daemon instead of events if it is ok to kill thread abruptly.
+    #t.daemon = True
     t.start()
 
 for i in range(100):
     q.put(i)
 
 q.join()
+print "thread count: {}".format(threading.active_count())
+
+done_event.set()
