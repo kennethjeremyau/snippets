6a7f2d593fbff63f5581c544c9ad567307270fec
Kenny Au <kennethjeremyau@gmail.com>
2019-05-30 14:46:09 -0600

    Added boto3 error handling to s3 download example.

--- python/aws/s3/download.py
+++ python/aws/s3/download.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
 import boto3
+from botocore.exceptions import ClientError
 
 s3 = boto3.client('s3')
 
-s3.download_file('bucket', 'file.txt', './file.txt')
+try:
+    s3.download_file('bucket', 'file.txt', './file.txt')
+except ClientError as e:
+    if e.response['Error']['Code'] == '403':
+        print 'ERROR forbidden: {}'.format(e.response['Error']['Message'])
+    elif e.response['Error']['Code'] == '404':
+        print 'ERROR not found: {}'.format(e.response['Error']['Message'])
+    else:
+        raise e
