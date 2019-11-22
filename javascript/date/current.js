207ea6cc73d3114196ad6a179ea98affbf2ccab5
Kenny Au <ken@gfycat.com>
2019-11-22 14:06:35 -0700

    Comment was incorrect.

--- javascript/date/current.js
+++ javascript/date/current.js
@@ -1,15 +1,21 @@
-d = new Date("2017-01-01");
+let d = new Date("2017-01-01");
 console.log(d);
-d = new Date("2017-01-01T01:00:00");
-console.log(d);
-// Removing the 'T' makes it local time.
+
 d = new Date("2017-01-01 01:00:00");
 console.log(d);
+
+// ISO 8601
+d = new Date("2017-01-01T01:00:00");
+console.log(d);
+
 d = new Date("2017-01-01T01:00:00-07:00");
 console.log(d);
+
 d = new Date("2017", "1" - 1, "1");
 console.log(d);
+
 d = new Date(2017, 0, 1);
 console.log(d);
+
 d = new Date (2017, 0, -5);
 console.log(d);
