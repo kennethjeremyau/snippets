a4baa799ba5408e559e52ec60ebc0990c0e3e821
Kenny Au <ken@gfycat.com>
2020-05-21 15:42:20 -0600

    Python s3 presigned url example.

--- python/aws/s3/presigned_url.py
+++ python/aws/s3/presigned_url.py
@@ -1,8 +1,13 @@
 import boto3
+import requests
 
 url = boto3.client('s3').generate_presigned_url(
-        'put_object',
-        dict(Bucket='bucket', Key='key'),
-    )
+    'put_object',
+    dict(Bucket='BUCKET', Key='KEY'),
+)
 
 print(url)
+
+with open('file.txt', 'rb') as f:
+    r = requests.put(url, data=f)
+    r.raise_for_status()
