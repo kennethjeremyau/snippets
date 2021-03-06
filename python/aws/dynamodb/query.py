d691da3e29252829ca2a26580b94a67747384129
Kenny Au <kau@snapchat.com>
2021-03-05 22:31:26 -0700

    Expanded python dynamodb query example.

--- python/aws/dynamodb/query.py
+++ python/aws/dynamodb/query.py
@@ -7,9 +7,13 @@ table = dynamodb.Table('TABLE')
 
 try:
     res = table.query(
-        KeyConditionExpression='#id = :id',
-        ExpressionAttributeNames={ '#id': 'id' },
-        ExpressionAttributeValues={ ':id': 'ID' },
+        # IndexName = 'INDEX'
+        KeyConditionExpression = '#id = :id',
+        # KeyConditionExpression = '#id = :id AND #sort <= :sort',
+        # KeyConditionExpression = '#id = :id AND #sort BETWEEN :sort1 AND :sort2',
+        # KeyConditionExpression = '#id = :id AND begins_with(#sort, :sort)',
+        ExpressionAttributeNames = { '#id': 'id' },
+        ExpressionAttributeValues = { ':id': 'ID' },
     )
     items = [ item['id'] for item in res['Items'] ]
     print (items)
