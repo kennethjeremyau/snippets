97215275a57daa7b15bb2f88640c9b0cb000e9e8
Kenny Au <kennethjeremyau@gmail.com>
2022-09-18 13:34:14 -0700

    Add code comments.

--- javascript/game/gameoflife/script.js
+++ javascript/game/gameoflife/script.js
@@ -1,16 +1,38 @@
+// Display width of the game board.
 var DISPLAY_WIDTH = 100;
+
+// Display height of the game board.
 var DISPLAY_HEIGHT = 100;
+
+// Width of each cell.
 var CELLPADDING = 5;
+
+// Number of cells across the board.
 var WIDTH = Math.floor(DISPLAY_WIDTH / CELLPADDING);
+
+// Number of cells along the board.
 var HEIGHT = Math.floor(DISPLAY_HEIGHT / CELLPADDING);
+
+// Frames per second.
 var FPS = 1;
 
+// Enum representing a dead cell.
+var CELLSTATE_DEAD = 0;
+
+// Enum representing a living cell.
+var CELLSTATE_ALIVE = 1;
+
+// Global game state. This is global because window.requestAnimationFrame
+// cannot be passed any variables.
 var gameState = {
     ctx: null,
     cells: [],
     lastUpdateTimestamp: 0
 };
 
+/**
+* main is the main function.
+*/
 function main() {
     var canvas = document.getElementById("canvas");
     gameState.ctx = canvas.getContext("2d");
@@ -19,13 +41,31 @@ function main() {
     window.requestAnimationFrame(loop);
 }
 
+/**
+* init initializes the game.
+*/
 function init(ctx) {
+    initCells();
     drawGridLines(ctx);
 }
 
+/**
+* initCells allocates and initializes the game state 2D array of cells.
+* All cells start of dead.
+*/
 function initCells() {
+    for (var i = 0; i < HEIGHT; i++) {
+        var row = [];
+        for (var j = 0; j < WIDTH; j++) {
+            row[j] = CELLSTATE_DEAD;
+        }
+        gameState.cells[i] = row;
+    }
 }
 
+/** 
+* loop is the main game loop as dictated by the browser.
+*/
 function loop(timestamp) {
     window.requestAnimationFrame(loop);
 
@@ -39,6 +79,9 @@ function loop(timestamp) {
     gameState.lastUpdateTimestamp = timestamp;
 }
 
+/**
+* drawGridLines draws the lines of the grid on the canvas.
+*/
 function drawGridLines(ctx) {
     for (var x = 0; x < DISPLAY_WIDTH; x += CELLPADDING) {
         ctx.moveTo(x + CELLPADDING, 0);
@@ -52,6 +95,9 @@ function drawGridLines(ctx) {
     ctx.stroke();
 }
 
+/**
+* drawCells draws the cells on the canvas.
+*/
 function drawCells(ctx) {
     // TODO
 }
