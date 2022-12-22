be37089f7f38f4b3a924b0cee5107f4cf2bbe913
Kenny Au <kennethjeremyau@gmail.com>
2022-12-22 13:45:31 -0800

    Access the rabbitmq gui.

--- k8s/rabbitmq/README.md
+++ k8s/rabbitmq/README.md
@@ -3,3 +3,4 @@
 ## Installation (OSX)
 1. Start Docker for Mac.
 1. `./install.sh`
+1. (Optional) `minikube tunnel` in another terminal to access the GUI at localhost:15672.
--- k8s/rabbitmq/install.sh
+++ k8s/rabbitmq/install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# Ensure k8s is running.
 minikube status || minikube start
+
+# Install RabbitMQ Cluster Operator
+# This is a k8s plugin which allows us to use the rabbitmqcluster kind.
 kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
+
+# Create the RabbitMQ servers.
+kubectl apply -f definition.yaml
+
+# Display GUI credentials
+echo GUI Username:
+kubectl get secret rabbitmq-example-default-user -o jsonpath="{.data.username}" | base64 --decode
+echo
+echo
+echo GUI Password:
+kubectl get secret rabbitmq-example-default-user -o jsonpath="{.data.password}" | base64 --decode
+
